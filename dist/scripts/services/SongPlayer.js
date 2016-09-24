9bbf7da8354ce9c708aa4f5a3483602846deea38
Jeffrey King <jdking33@aol.com>
2016-09-24 08:22:45 -0700

    Submit Assignment 7-Add playSong function

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -2,7 +2,12 @@
   function SongPlayer() {
     var SongPlayer = {};
 
+    /**
+    * @desc variable that stores the currently selected song
+    * @type {object}
+    */
     var currentSong = null;
+
     /**
     * @desc Buzz object audio file
     * @type {object}
@@ -28,22 +33,38 @@
       currentSong = song;
     }
 
+    /**
+    * @desc calls for passed song to be played and, if different than current
+    *       song, calls for the passed song to be set current
+    * @param [object] song
+    */
     SongPlayer.play = function(song) {
       if(currentSong !== song) {       
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
       } else if(currentSong === song) {
           if(currentBuzzObject.isPaused()) {
-            currentBuzzObject.play();
-            song.playing = true;
+          playSong(song);
           }
       }
     };
 
+    /**
+    * @desc pauses the song, sets "playing" attribute to false
+    * @param [object] song
+    */
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
+    }
+
+    /**
+    * @desc plays the song, sets "playing" attribute to true
+    * @param [object] song
+    */
+    var playSong = function(song) {
+      currentBuzzObject.play();
+      song.playing = true;
     }
 
     return SongPlayer;
